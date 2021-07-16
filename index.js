5cecb87be8872eb1bd060a8981f26a0226acc1ff
mzidani <mzidani@sqli.com>
2021-07-16 01:45:24 +0100

    post

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ app.get("/", function (req, res) {
   res.send("<h1>Hello World!</h1>")
 })
 
+app.post('/webhooks/orders/create', (req, res) => {
+    console.log('🎉 We got an order!')
+    res.sendStatus(200)
+})
+
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
